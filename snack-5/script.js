dda19d597d34c8cf4e45c00fd16a1d9b452f178a
matteolpera <matteo.lepera02@gmail.com>
2025-11-13 15:36:26 +0100

    script 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,12 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// let numsEven = nums.filter(function (number) {
+//     return number % 2 === 0;
+// });
+// console.log(numsEven);
+
+//arrow function
+let numsEven = nums.filter((number) => number % 2 === 0);
+console.log(numsEven);
